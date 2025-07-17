94437bbeab36a542aaf2fe53cf075bc25175fc78
KingEddeh <28296038+KingEddeh@users.noreply.github.com>
2025-07-17 18:54:17 +0800

    feat: register User model in admin and fix ALLOWED_HOSTS environment variable handling

--- backend/nstw_backend/settings.py
+++ backend/nstw_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
